87ff47a9b7b66b563cb55fe8a4b046f13eff6dc5
Kishan Akbari <kishanakbari8888@gmail.com>
2023-07-24 01:54:45 +0530

    version 3.1

--- routes/journalUpdate.js
+++ routes/journalUpdate.js
@@ -9,84 +9,98 @@ const { upload } = require('../extrafunction/uploadFile');
 
 router.put('/description',teacherAuthorization,async (req,res)=>{
     const {postid,description} = req.body;
-
-    // console.log(postid,description);
-    await connection.query(`UPDATE post SET description = "${description}" WHERE postid = "${postid}"`,async (err,result)=>{
+    await connection.query(`SELECT * FROM post WHERE postid = "${postid}" AND teacherid = "${req.user.name}"`,async (err,result)=>{
         if(err){
-            console.log(err);
             return res.status(500).send('Server error');
         }
-        return res.status(200).send('description updated');
+        if(result.length == 0){
+            return res.status(400).send('You dont have this post');   
+        }
+        
+        await connection.query(`UPDATE post SET description = "${description}" WHERE postid = "${postid}"`,async (err,result)=>{
+            if(err){
+                console.log(err);
+                return res.status(500).send('Server error');
+            }
+            return res.status(200).send('description updated');
+        });
+        
     });
-
 });
 
 router.put('/addstudent',teacherAuthorization,async (req,res)=>{
     
     const {postid,studentids} = req.body;
-    // console.log(postid,studentids);
-    
-    let error = {succfulyadded:[],alreadyindb:[],notfound:[]};
-    const allpromise = [];
 
-    studentids.forEach(async (element) => {
+    await connection.query(`SELECT * FROM post WHERE postid = "${postid}" AND teacherid = "${req.user.name}"`,async (err,result)=>{
+        if(err){
+            return res.status(500).send('Server error');
+        }
+        if(result.length == 0){
+            return res.status(400).send('You dont have this post');
 
-        let promise = new Promise(async (resolve,reject)=>{
-            await connection.query(`SELECT * FROM student WHERE userid = "${element}"`,async (err,result)=>{
-                if(err){
-                    return reject(err);
-                }
-                if(result.length == 0){
-                    return resolve([0,element]);
-                }
+        }
+        
+        let error = {succfulyadded:[],alreadyindb:[],notfound:[]};
+        const allpromise = [];
 
-                await connection.query(`SELECT * FROM tag WHERE postid = "${postid}" AND studentid = "${element}"`,async (err,result)=>{
+        studentids.forEach(async (element) => {
+
+            let promise = new Promise(async (resolve,reject)=>{
+                await connection.query(`SELECT * FROM student WHERE userid = "${element}"`,async (err,result)=>{
                     if(err){
                         return reject(err);
                     }
-                    if(result.length != 0){
-                        return resolve([2,element]);
+                    if(result.length == 0){
+                        return resolve([0,element]);
                     }
 
-                });
+                    await connection.query(`SELECT * FROM tag WHERE postid = "${postid}" AND studentid = "${element}"`,async (err,result)=>{
+                        if(err){
+                            return reject(err);
+                        }
+                        if(result.length != 0){
+                            return resolve([2,element]);
+                        }
+
+                    });
 
 
 
-                await connection.query(`INSERT INTO tag(postid,studentid) VALUES ("${postid}","${element}")`,async (err,result)=>{
-                    if(err){
-                        return reject(err);
-                    }
-                    return resolve([1,element]);
+                    await connection.query(`INSERT INTO tag(postid,studentid) VALUES ("${postid}","${element}")`,async (err,result)=>{
+                        if(err){
+                            return reject(err);
+                        }
+                        return resolve([1,element]);
+                    });
                 });
             });
-        });
 
-        allpromise.push(promise);
+            allpromise.push(promise);
+        });
+        
+        try{
+            await Promise.all(allpromise).then((data)=>{
+                // console.log(data);
+                data.forEach((element)=>{
+                    if(element[0] == 1){
+                        error.succfulyadded.push(element[1]);
+                    }
+                    else if(element[0] == 2){
+                        error.alreadyindb.push(element[1]);
+                    }
+                    else{
+                        error.notfound.push(element[1]);
+                    }
+                });
+            });
+        }
+        catch(err){
+            return res.status(500).send({error:"server error or some student already added"});
+        }
+        
+        return res.status(200).json(error);
     });
-    
-    try{
-        await Promise.all(allpromise).then((data)=>{
-            // console.log(data);
-            data.forEach((element)=>{
-                if(element[0] == 1){
-                    error.succfulyadded.push(element[1]);
-                }
-                else if(element[0] == 2){
-                    error.alreadyindb.push(element[1]);
-                }
-                else{
-                    error.notfound.push(element[1]);
-                }
-            });
-        });
-    }
-    catch(err){
-        return res.status(500).send({error:"server error or some student already added"});
-    }
-
-    // console.log(error);
-
-    return res.status(200).json(error);
     
 });
 
